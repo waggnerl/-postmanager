4a00b602b1a6ae8dd2a4e03e165f90856cd49b47
waggnerl <wagnermflira@gmail.com>
2023-06-19 04:32:24 -0300

    feat:Include NavBar signout feature

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,9 +1,22 @@
+import { updateUser } from "../../actions/user";
+import { useAppDispatch } from "../../redux/store";
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(updateUser(""));
+    localStorage.setItem("username", "");
+    console.log("hey");
+    navigate("/");
+  };
   return (
     <nav>
       <p>CodeLeap Network</p>
+      <img src="/logout.svg" height={"20px"} alt="" onClick={handleLogout} />
     </nav>
   );
 }
